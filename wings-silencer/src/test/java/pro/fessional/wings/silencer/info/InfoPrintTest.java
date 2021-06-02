685cb056ac863c27bfa50c8afc016fa565a670cf
trydofor <trydofor@gmail.com>
2021-06-02 17:16:33 +0800

    [🐵新增] 增加build和git Info [🐵调整] 调整actuator到slardar,code style

---
+++ wings-silencer/src/test/java/pro/fessional/wings/silencer/info/InfoPrintTest.java
@@ -0,0 +1,28 @@
+package pro.fessional.wings.silencer.info;
+
+import lombok.Setter;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.boot.info.GitProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author trydofor
+ * @since 2021-06-02
+ */
+@SpringBootTest
+public class InfoPrintTest {
+
+    @Setter(onMethod_ = {@Autowired})
+    private BuildProperties buildProperties;
+
+    @Setter(onMethod_ = {@Autowired})
+    private GitProperties gitProperties;
+
+    @Test
+    public void printTest(){
+        System.out.println(buildProperties);
+        System.out.println(gitProperties);
+    }
+}
---
+++ wings-slardar/src/main/java/pro/fessional/wings/slardar/spring/bean/SlardarActuatorConfiguration.java
@@ -0,0 +1,17 @@
+package pro.fessional.wings.slardar.spring.bean;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * @author trydofor
+ * @since 2019-12-01
+ */
+@Configuration
+public class SlardarActuatorConfiguration {
+
+    private final static Log logger = LogFactory.getLog(SlardarActuatorConfiguration.class);
+
+}
