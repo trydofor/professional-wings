d0cd198969f22ce578c541908b59aa4a9017f861
trydofor <trydofor@gmail.com>
2021-07-21 08:45:51 +0800

    [🐵新增] 消除，且可忽略一些已知非关键WARN

--- wings-slardar/src/main/java/pro/fessional/wings/slardar/monitor/filtter/LogViewer.java
+++ wings-slardar/src/main/java/pro/fessional/wings/slardar/monitor/filtter/LogViewer.java
@@ -4,6 +4,7 @@ import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import io.swagger.annotations.ApiOperation;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -15,12 +16,18 @@ import pro.fessional.wings.slardar.monitor.WarnFilter;
 import pro.fessional.wings.slardar.monitor.WarnMetric;
 import pro.fessional.wings.slardar.spring.prop.SlardarMonitorProp;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -29,6 +36,7 @@ import java.util.UUID;
  * @author trydofor
  * @since 2021-07-20
  */
+@Slf4j
 @RestController
 @ConditionalOnProperty(name = LogViewer.Conf.Key$enable, havingValue = "true")
 public class LogViewer implements WarnFilter {
@@ -59,8 +67,14 @@ public class LogViewer implements WarnFilter {
         if (log == null) return;
         File file = new File(log);
         if (!file.canRead()) return;
+
         try (FileInputStream fis = new FileInputStream(file)) {
-            IOUtils.copyLarge(fis, res.getOutputStream(), 0L, conf.length.toBytes());
+            final long len = conf.length.toBytes();
+            final ServletOutputStream outputStream = res.getOutputStream();
+            IOUtils.copyLarge(fis, res.getOutputStream(), 0L, len);
+            if (file.length() - len > 0) {
+                outputStream.write(("\n\n......" + len + "/" + file.length() + " bytes").getBytes());
+            }
         }
     }
 
@@ -68,19 +82,29 @@ public class LogViewer implements WarnFilter {
     public void filter(Map<String, List<WarnMetric.Warn>> warns) {
         List<WarnMetric.Warn> flt = new ArrayList<>();
         for (List<WarnMetric.Warn> list : warns.values()) {
-            for (WarnMetric.Warn next : list) {
+            for (Iterator<WarnMetric.Warn> iter = list.iterator(); iter.hasNext(); ) {
+                WarnMetric.Warn next = iter.next();
                 if (next.getType() == WarnMetric.Type.File) {
-                    WarnMetric.Warn wd = new WarnMetric.Warn();
-                    wd.setType(WarnMetric.Type.Link);
-                    wd.setKey(next.getKey());
-                    wd.setRule(next.getRule());
-                    final String id = UUID.randomUUID().toString();
-                    cache.put(id, next.getWarn());
-                    wd.setWarn(conf.domain + conf.mapping + "?id=" + id);
-                    flt.add(wd);
+                    if (canIgnoreHead(next.getWarn())) {
+                        log.info("remove ignored warning");
+                        iter.remove();
+                    }
+                    else {
+                        WarnMetric.Warn wd = new WarnMetric.Warn();
+                        wd.setType(WarnMetric.Type.Link);
+                        wd.setKey(next.getKey());
+                        wd.setRule(next.getRule());
+                        final String id = UUID.randomUUID().toString();
+                        cache.put(id, next.getWarn());
+                        wd.setWarn(conf.domain + conf.mapping + "?id=" + id);
+                        flt.add(wd);
+                    }
                 }
             }
         }
+
+        warns.entrySet().removeIf(it -> it.getValue().isEmpty());
+
         if (!flt.isEmpty()) {
             final List<WarnMetric.Warn> old = warns.get(Conf.Key);
             if (old == null) {
@@ -89,6 +113,39 @@ public class LogViewer implements WarnFilter {
             else {
                 old.addAll(flt);
             }
+        }
+    }
+
+    private boolean canIgnoreHead(String out) {
+        if (conf.ignore.isEmpty()) return false;
+        long max = conf.length.toBytes();
+        final File file = new File(out);
+        if (file.length() > max || !file.canRead()) return false;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            final Collection<String> ign = conf.ignore.values();
+            String line;
+            int tol = 0;
+            int cnt = 0;
+            out:
+            while ((line = reader.readLine()) != null && max > 0) {
+                if (line.isEmpty()) {
+                    continue;
+                }
+                //
+                max -= line.length(); // 不精确计算
+                tol++;
+                for (String s : ign) {
+                    if (line.contains(s)) {
+                        cnt++;
+                        continue out;
+                    }
+                }
+            }
+            return tol == cnt;
+        }
+        catch (Exception e) {
+            return true;
         }
     }
 
@@ -131,5 +188,13 @@ public class LogViewer implements WarnFilter {
          */
         private String domain = "";
         public static final String Key$domain = Key + ".domain";
+
+        /**
+         * 可以排除的日志中的警报
+         *
+         * @see #Key$ignore
+         */
+        private Map<String, String> ignore = new HashMap<>();
+        public static final String Key$ignore = Key + ".ignore";
     }
 }
