693aa57a7edde56e1a1bf1ae32eef51f0ee555c9
trydofor <trydofor@gmail.com>
2021-10-08 19:57:27 +0800

    [🐵新增] cookie增加forward测试，改用OncePerRequestFilter

--- wings-slardar/src/main/java/pro/fessional/wings/slardar/servlet/cookie/WingsCookieFilter.java
+++ wings-slardar/src/main/java/pro/fessional/wings/slardar/servlet/cookie/WingsCookieFilter.java
@@ -1,33 +1,36 @@
 package pro.fessional.wings.slardar.servlet.cookie;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.servlet.filter.OrderedFilter;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.core.Ordered;
+import org.springframework.web.filter.OncePerRequestFilter;
 import pro.fessional.wings.slardar.servlet.WingsServletConst;
 import pro.fessional.wings.slardar.servlet.request.WingsRequestWrapper;
 import pro.fessional.wings.slardar.servlet.response.WingsResponseWrapper;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.function.Function;
 
 /**
  * @author trydofor
  * @since 2019-11-14
  */
 @RequiredArgsConstructor
-public class WingsCookieFilter implements OrderedFilter {
+public class WingsCookieFilter extends OncePerRequestFilter implements Ordered {
 
     private final WingsCookieInterceptor interceptor;
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(@NotNull HttpServletRequest req, @NotNull HttpServletResponse res, @NotNull FilterChain chain)
+            throws ServletException, IOException {
+
         if (interceptor.notIntercept()) {
             chain.doFilter(req, res);
             return;
@@ -35,16 +38,16 @@ public class WingsCookieFilter implements OrderedFilter {
 
         WingsRequestWrapper request = WingsRequestWrapper.infer(req);
         if (request == null) {
-            request = new WingsRequestWrapper((HttpServletRequest) req);
+            request = new WingsRequestWrapper(req);
         }
 
 
         WingsResponseWrapper response = WingsResponseWrapper.infer(res);
         if (response == null) {
-            response = new WingsResponseWrapper((HttpServletResponse) res);
+            response = new WingsResponseWrapper(res);
         }
 
-
+        // read
         final Cookie[] ckOld = request.getCookies();
         if (ckOld != null && ckOld.length > 0) {
             final Cookie[] ckNew = Arrays
@@ -55,7 +58,14 @@ public class WingsCookieFilter implements OrderedFilter {
             request.setCookies(ckNew);
         }
 
-        response.setCookieInterceptor(interceptor::write);
+        // write
+        final Function<Cookie, Cookie> ciOld = response.getCookieInterceptor();
+        if (ciOld == null) {
+            response.setCookieInterceptor(interceptor::write);
+        }
+        else {
+            response.setCookieInterceptor(it -> interceptor.write(ciOld.apply(it)));
+        }
 
         chain.doFilter(request, response);
     }
--- wings-slardar/src/test/java/pro/fessional/wings/slardar/webmvc/WingsCookieTest.java
+++ wings-slardar/src/test/java/pro/fessional/wings/slardar/webmvc/WingsCookieTest.java
@@ -2,8 +2,14 @@ package pro.fessional.wings.slardar.webmvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Setter;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -14,12 +20,18 @@ import pro.fessional.wings.slardar.controller.TestCookieController.Ins;
 import pro.fessional.wings.slardar.spring.prop.SlardarCookieProp;
 
 import javax.servlet.http.Cookie;
+import java.util.HashSet;
+import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static pro.fessional.wings.slardar.httprest.OkHttpClientHelper.APPLICATION_JSON_VALUE;
 
 /**
  * @author trydofor
@@ -50,12 +62,29 @@ public class WingsCookieTest {
     @Setter(onMethod_ = {@Autowired})
     private ObjectMapper objectMapper;
 
+    @Setter(onMethod_ = {@Value("http://127.0.0.1:${local.server.port}")})
+    private String domain;
+
+    @Setter(onMethod_ = {@Autowired})
+    private OkHttpClient okHttpClient;
+
     @Test
     public void test1Cookie() throws Exception {
+        mockMvcCookie("/test/cookie.json");
+    }
 
+    @Test
+    public void test2Cookie() throws Exception {
+        // 必须用 client，mock不执行
+        httpClient("/test/cookie-forward.json");
+        // mock mvc 并不真正执行 mapping方法
+//        mockMvcCookie("/test/cookie-forward.json");
+    }
+
+    public void mockMvcCookie(String url) throws Exception {
         final Ins ins = new Ins();
-        ins.setCk1("ck1 is 好人");
-        ins.setCk2("ck2 is good");
+        ins.setCk1("ck1-is-man");
+        ins.setCk2("ck2-is-good");
         ins.setB64("base 64");
         ins.setAes("aes 128");
         ins.setOth("other");
@@ -63,7 +92,7 @@ public class WingsCookieTest {
         Aes128 aes128 = Aes128.of(prop.getAesKey());
 
         mockMvc.perform(
-                post("/test/cookie.json")
+                post(url)
                         .contentType(MediaType.APPLICATION_JSON)
                         .cookie(new Cookie(PREFIX + "ck1", ins.getCk1()),
                                 new Cookie(PREFIX + CK2OTH, ins.getCk2()))
@@ -75,8 +104,41 @@ public class WingsCookieTest {
                .andExpect(cookie().value(PREFIX + CK2OTH, ins.getCk2()))
                .andExpect(cookie().value(PREFIX + "b64", Base64.encode(ins.getB64())))
                .andExpect(cookie().value(PREFIX + "aes", aes128.encode64(ins.getAes())))
+               .andExpect(cookie().value(PREFIX + "oth", aes128.encode64(ins.getOth())))
                .andExpect(content().string(ins.getCk1() + ins.getCk2()))
         ;
     }
 
+    public void httpClient(String url) throws Exception {
+        final Ins ins = new Ins();
+        ins.setCk1("ck1-is-man");
+        ins.setCk2("ck2-is-good");
+        ins.setB64("base 64");
+        ins.setAes("aes 128");
+        ins.setOth("other");
+
+        Aes128 aes128 = Aes128.of(prop.getAesKey());
+
+        String cookieValue = PREFIX + "ck1=" + ins.getCk1() + "; " + PREFIX + CK2OTH + "=" + ins.getCk2();
+
+        RequestBody body = RequestBody.create(objectMapper.writeValueAsString(ins), APPLICATION_JSON_VALUE);
+        Request request = new Request.Builder()
+                .url(domain + url)
+                .addHeader("Cookie", cookieValue)
+                .post(body)
+                .build();
+        try (Response response = okHttpClient.newCall(request).execute()) {
+            final ResponseBody resBody = response.body();
+            assertEquals(200, response.code());
+            assertNotNull(resBody);
+            assertEquals(ins.getCk1() + ins.getCk2(), resBody.string());
+
+            final Set<String> cookies = new HashSet<>(response.headers("Set-Cookie"));
+            assertTrue(cookies.contains(PREFIX + "ck1=" + ins.getCk1()));
+            assertTrue(cookies.contains(PREFIX + CK2OTH + "=" + ins.getCk2()));
+            assertTrue(cookies.contains(PREFIX + "b64=" + Base64.encode(ins.getB64())));
+            assertTrue(cookies.contains(PREFIX + "aes=" + aes128.encode64(ins.getAes())));
+            assertTrue(cookies.contains(PREFIX + "oth=" + aes128.encode64(ins.getOth())));
+        }
+    }
 }
