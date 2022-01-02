1913295fc68fd65edc835d7fe8333f24673e49af
trydofor <trydofor@gmail.com>
2022-01-02 14:34:36 +0800

    ✨ mysql脚本，支持忽略空密码

--- wings-example/demo-devops/bin/wings-mysql-user.sh
+++ wings-example/demo-devops/bin/wings-mysql-user.sh
@@ -17,9 +17,12 @@ cat << EOF
 #################################################
 EOF
 
+function passwd24() {
+  head /dev/urandom | LC_ALL=C tr -dc A-Za-z0-9 | head -c 24
+}
+
 #####
 execute=false
-database=""
 command="$1"
 config="$3"
 
@@ -27,6 +30,7 @@ if [[ "$command" == "" || "$command" == "help" ]]; then
   # https://dev.mysql.com/doc/refman/5.7/en/account-management-statements.html
   echo -e '\033[37;42;1mNOTE: users env file\033[m'
   echo 'execute=true # 非true时，仅显示sql而不执行'
+  echo '# passwd 空为忽略'
   echo 'database=数据库'
   echo 'pass_raw=密码'
   echo 'pass_app=密码'
@@ -48,43 +52,23 @@ if [[ -f "$2" ]]; then
   source "$2"
 fi
 
+declare database
 if [[ "$database" == "" ]]; then
   echo -e "\033[37;41;1mERROR: need database in users config \033[0m"
   exit
 fi
 
 #
-len=24
-if [[ "$pass_raw" == "" ]]; then
-  echo "set random password to raw"
-  pass_raw=$(head /dev/urandom | LC_ALL=C tr -dc A-Za-z0-9 | head -c $len)
-fi
-if [[ "$pass_app" == "" ]]; then
-  echo "set random password to app"
-  pass_app=$(head /dev/urandom | LC_ALL=C tr -dc A-Za-z0-9 | head -c $len)
-fi
-if [[ "$pass_dev" == "" ]]; then
-  echo "set random password to dev"
-  pass_dev=$(head /dev/urandom | LC_ALL=C tr -dc A-Za-z0-9 | head -c $len)
-fi
-if [[ "$pass_dba" == "" ]]; then
-  echo "set random password to dba"
-  pass_dba=$(head /dev/urandom | LC_ALL=C tr -dc A-Za-z0-9 | head -c $len)
-fi
+[[ "$pass_raw" == "" ]] && user_raw='#' pass_raw=$(passwd24)
+[[ "$pass_app" == "" ]] && user_app='#' pass_app=$(passwd24)
+[[ "$pass_dev" == "" ]] && user_dev='#' pass_dev=$(passwd24)
+[[ "$pass_dba" == "" ]] && user_dba='#' pass_dba=$(passwd24)
 
 #
-if [[ "$host_raw" == "" ]]; then
-  host_raw=%
-fi
-if [[ "$host_app" == "" ]]; then
-  host_app=%
-fi
-if [[ "$host_dev" == "" ]]; then
-  host_dev=%
-fi
-if [[ "$host_dba" == "" ]]; then
-  host_dba=%
-fi
+[[ "$host_raw" == "" ]] && host_raw=%
+[[ "$host_app" == "" ]] && host_app=%
+[[ "$host_dev" == "" ]] && host_dev=%
+[[ "$host_dba" == "" ]] && host_dba=%
 
 exec_cmd="cat"
 if [[ "$execute" == "true" ]]; then
@@ -98,39 +82,39 @@ if [[ "$execute" == "true" ]]; then
 fi
 
 echo -e '\033[37;42;1mNOTE: users and passwd\033[m'
-cat << EOF
-$database.raw  $pass_raw
-$database.app  $pass_app
-$database.dev  $pass_dev
-$database.dba  $pass_dba
+grep -v '^#' << EOF
+${user_raw}$database.raw  $pass_raw
+${user_app}$database.app  $pass_app
+${user_dev}$database.dev  $pass_dev
+${user_dba}$database.dba  $pass_dba
 EOF
 
 echo -e '\033[37;42;1mNOTE: sql script to execute\033[m'
 
 if [[ "$command" == "create" ]]; then
 db_main=${database//_/\\_}
-$exec_cmd << EOF
+grep -v '^#' << EOF | tee /dev/tty | $exec_cmd
 -- create
-CREATE USER '$database.raw'@'$host_raw' IDENTIFIED BY '$pass_raw';
-CREATE USER '$database.app'@'$host_app' IDENTIFIED BY '$pass_app';
-CREATE USER '$database.dev'@'$host_dev' IDENTIFIED BY '$pass_dev';
-CREATE USER '$database.dba'@'$host_dba' IDENTIFIED BY '$pass_dba';
+${user_raw}CREATE USER '$database.raw'@'$host_raw' IDENTIFIED BY '$pass_raw';
+${user_app}CREATE USER '$database.app'@'$host_app' IDENTIFIED BY '$pass_app';
+${user_dev}CREATE USER '$database.dev'@'$host_dev' IDENTIFIED BY '$pass_dev';
+${user_dba}CREATE USER '$database.dba'@'$host_dba' IDENTIFIED BY '$pass_dba';
 -- grant
-GRANT SELECT, CREATE TEMPORARY TABLES ON \`$db_main%\`.* TO '$database.raw'@'$host_raw';
-GRANT SELECT, CREATE TEMPORARY TABLES, INSERT, UPDATE, DELETE, EXECUTE ON \`$db_main%\`.* TO '$database.app'@'$host_app';
-GRANT ALL ON \`$db_main%\`.* TO '$database.dev'@'$host_dev';
-REVOKE DROP ON \`$db_main%\`.* FROM '$database.dev'@'$host_dev';
-GRANT ALL ON \`$db_main%\`.* TO '$database.dba'@'$host_dba';
+${user_raw}GRANT SELECT, CREATE TEMPORARY TABLES ON \`$db_main%\`.* TO '$database.raw'@'$host_raw';
+${user_app}GRANT SELECT, CREATE TEMPORARY TABLES, INSERT, UPDATE, DELETE, EXECUTE ON \`$db_main%\`.* TO '$database.app'@'$host_app';
+${user_dev}GRANT ALL ON \`$db_main%\`.* TO '$database.dev'@'$host_dev';
+${user_dev}REVOKE DROP ON \`$db_main%\`.* FROM '$database.dev'@'$host_dev';
+${user_dba}GRANT ALL ON \`$db_main%\`.* TO '$database.dba'@'$host_dba';
 EOF
 fi
 
 if [[ "$command" == "passwd" ]]; then
-$exec_cmd << EOF
+grep -v '^#' << EOF | tee /dev/tty | $exec_cmd
 -- change passwd
-ALTER USER '$database.raw'@'$host_raw' IDENTIFIED BY '$pass_raw';
-ALTER USER '$database.app'@'$host_app' IDENTIFIED BY '$pass_app';
-ALTER USER '$database.dev'@'$host_dev' IDENTIFIED BY '$pass_dev';
-ALTER USER '$database.dba'@'$host_dba' IDENTIFIED BY '$pass_dba';
+${user_raw}ALTER USER '$database.raw'@'$host_raw' IDENTIFIED BY '$pass_raw';
+${user_app}ALTER USER '$database.app'@'$host_app' IDENTIFIED BY '$pass_app';
+${user_dev}ALTER USER '$database.dev'@'$host_dev' IDENTIFIED BY '$pass_dev';
+${user_dba}ALTER USER '$database.dba'@'$host_dba' IDENTIFIED BY '$pass_dba';
 EOF
 fi
 
