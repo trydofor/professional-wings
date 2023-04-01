daa6515ebf74490b4ebc12651aa57f967fb770ba
trydofor <trydofor@gmail.com>
2023-04-01 19:32:04 +0800

    ✨ Happy April Fools' Day, improve progress bar

--- wings/slardar/src/main/java/pro/fessional/wings/slardar/concur/ProgressContext.java
+++ wings/slardar/src/main/java/pro/fessional/wings/slardar/concur/ProgressContext.java
@@ -5,11 +5,13 @@ import org.cache2k.Cache;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import pro.fessional.mirana.code.LeapCode;
-import pro.fessional.wings.slardar.cache.cache2k.Cache2kSlot;
+import pro.fessional.mirana.time.ThreadNow;
 
 
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
+
+import static pro.fessional.wings.slardar.cache.cache2k.Cache2kSlot.H24M5;
 
 /**
  * @author trydofor
@@ -17,7 +19,6 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 public class ProgressContext {
 
-    private static final Cache2kSlot slots = new Cache2kSlot(24 * 3600, 300);
     private static final AtomicLong count = new AtomicLong(0);
     private static final LeapCode leapCode = new LeapCode();
 
@@ -47,7 +48,7 @@ public class ProgressContext {
     public static Bar get(Object key, int second) {
         if (key == null) return null;
 
-        final Cache<Object, Object> cache = slots.getCache(second);
+        final Cache<Object, Object> cache = H24M5.getCache(second);
         final Object obj = cache.get(key);
         if (obj instanceof Bar) {
             return (Bar) obj;
@@ -65,7 +66,12 @@ public class ProgressContext {
      */
     @NotNull
     public static Cache<Object, Object> get(int second) {
-        return slots.getCache(second);
+        return H24M5.getCache(second);
+    }
+
+    @NotNull
+    public static Bar gen(Object key, int second) {
+        return gen(key, ThreadNow.millis(), second);
     }
 
     /**
@@ -78,7 +84,7 @@ public class ProgressContext {
      */
     @NotNull
     public static Bar gen(Object key, long started, int second) {
-        final Cache<Object, Object> cache = slots.getCache(second);
+        final Cache<Object, Object> cache = H24M5.getCache(second);
         Bar bar = new Bar(key(started, second), started);
         cache.put(bar.key, bar);
         if (key != null) {
@@ -115,9 +121,10 @@ public class ProgressContext {
 
         private final String key;
         private final long started;
-
-        private transient volatile boolean done = false;
+        private volatile long stopped = -1;
+        private volatile int percent = 0;
         private transient volatile Object result = null;
+        private transient volatile Object attach = null;
         private transient volatile Throwable error = null;
 
         public Bar(String key, long started) {
@@ -125,14 +132,33 @@ public class ProgressContext {
             this.started = started;
         }
 
+        public boolean isDone() {
+            return stopped > 0;
+        }
+
+        /**
+         * set current progress percent, from 0 to 100
+         */
+        public void setPercent(int percent) {
+            this.percent = percent;
+        }
+
+        /**
+         * set attachment to the bar
+         */
+        public void setAttach(Object attach) {
+            this.attach = attach;
+        }
+
         public void fail(Throwable error) {
             this.error = error;
-            this.done = true;
+            this.stopped = ThreadNow.millis();
         }
 
         public void ok(Object result) {
             this.result = result;
-            this.done = true;
+            this.stopped = ThreadNow.millis();
+            this.percent = 100;
         }
 
         @Override
@@ -148,13 +174,12 @@ public class ProgressContext {
             return Objects.hash(key);
         }
 
-        @Override
-        public String toString() {
+        @Override public String toString() {
             return "Bar{" +
                    "key='" + key + '\'' +
                    ", started=" + started +
-                   ", done=" + done +
-                   ", error=" + error +
+                   ", stopped=" + stopped +
+                   ", percent=" + percent +
                    '}';
         }
     }
