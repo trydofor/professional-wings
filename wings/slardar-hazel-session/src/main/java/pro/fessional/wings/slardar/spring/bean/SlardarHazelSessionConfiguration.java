e317192eca4db4e278ea53d36e8e4aebb2080367
trydofor <trydofor@gmail.com>
2024-03-18 11:20:17 +0800

    ➕ caching misdepends session #221

--- wings/slardar-hazel-session/src/main/java/pro/fessional/wings/slardar/spring/bean/SlardarHazelSessionConfiguration.java
+++ wings/slardar-hazel-session/src/main/java/pro/fessional/wings/slardar/spring/bean/SlardarHazelSessionConfiguration.java
@@ -1,13 +1,18 @@
 package pro.fessional.wings.slardar.spring.bean;
 
+import com.hazelcast.config.SerializationConfig;
+import com.hazelcast.config.SerializerConfig;
 import com.hazelcast.core.HazelcastInstance;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.hazelcast.HazelcastConfigCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.session.MapSession;
 import org.springframework.session.Session;
+import org.springframework.session.hazelcast.HazelcastSessionSerializer;
 import org.springframework.session.security.SpringSessionBackedSessionRegistry;
 import pro.fessional.wings.silencer.spring.boot.ConditionalWingsEnabled;
 import pro.fessional.wings.slardar.session.HazelcastSessionHelper;
@@ -38,5 +43,38 @@ public class SlardarHazelSessionConfiguration {
 
         log.info("SlardarHazelSession spring-bean wingsSessionHelper");
         return new HazelcastSessionHelper(sessionRepository, hazelcastInstance, mapName);
+    }
+
+    /**
+     * spring FindByIndexNameSessionRepository
+     */
+    @Bean
+    @ConditionalWingsEnabled
+    public HazelcastConfigCustomizer wingsHazelcastSessionSerializer() {
+        log.info("SlardarHazelSession spring-bean wingsHazelcastSessionSerializer");
+        return config -> {
+            String msc = MapSession.class.getName();
+            SerializationConfig serialization = config.getSerializationConfig();
+            for (SerializerConfig ss : serialization.getSerializerConfigs()) {
+                String tcn = ss.getTypeClassName();
+                if (tcn == null) {
+                    Class<?> tc = ss.getTypeClass();
+                    if (tc != null) {
+                        tcn = tc.getName();
+                    }
+                }
+
+                if (msc.equals(tcn)) {
+                    log.warn("Wings hazelcast addSerializerConfig skipped, current=" + ss);
+                    return;
+                }
+            }
+
+            log.info("Wings hazelcast addSerializerConfig type=MapSession");
+            SerializerConfig sessionSerializer = new SerializerConfig();
+            sessionSerializer.setClass(HazelcastSessionSerializer.class);
+            sessionSerializer.setTypeClass(MapSession.class);
+            serialization.addSerializerConfig(sessionSerializer);
+        };
     }
 }
